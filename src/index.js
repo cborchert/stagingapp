82132cb6f7e21e4a609c5fc10843da59ae44cf26
Chris Borchert <cborchertrun@gmail.com>
2018-07-21 19:08:09 +0200

    Correct CSS parsing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./index.css";
+
 const App = () => (
   <div className="App">
     <h1 className="App-Title">Hello Parcel x React</h1>
